f0e0c715402aa00cb41b9640e6fd427b9c865873
Amira Alzaian <amiraelzaian204@gmail.com>
2026-08-03 04:17:38 +0300

    feat: implement api features

--- controllers/factory.js
+++ controllers/factory.js
@@ -1,4 +1,5 @@
 const ApiError = require("../utils/apiError");
+const ApiFeatures = require("../utils/apiFeatures");
 
 exports.createOne = (Model) => async (req, res, next) => {
   const document = await Model.create(req.body);
@@ -9,12 +10,29 @@ exports.createOne = (Model) => async (req, res, next) => {
 };
 
 exports.getAll = (Model) => async (req, res, next) => {
-  const documents = await Model.find({}, { __v: false, password: false });
+  let filter = {};
+  if (req.filterObj) {
+    filter = req.filterObj;
+  }
+
+  const docsCount = await Model.countDocuments();
+  let apiFeatures = new ApiFeatures(Model.find(filter), req.query)
+    .paginate(docsCount)
+    .filter()
+    .search(Model.modelName)
+    .limitFields()
+    .sort();
+
+  const documents = await apiFeatures.mongooseQuery;
   if (!documents)
     return next(
       new ApiError(`Could not get documents for ${Model} model`, 404),
     );
-  res.status(200).json({ status: "success", data: documents });
+  res.status(200).json({
+    results: documents.length,
+    page: apiFeatures.paginationResult,
+    data: documents,
+  });
 };
 
 exports.getOne = (Model) => async (req, res, next) => {
---
+++ utils/apiFeatures.js
@@ -0,0 +1,88 @@
+class ApiFeatures {
+  constructor(mongooseQuery, queryString) {
+    this.mongooseQuery = mongooseQuery;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryStringObj = { ...this.queryString };
+    const excludedFields = ["page", "sort", "limit", "fields", "keyword"];
+    excludedFields.forEach((field) => delete queryStringObj[field]);
+
+    let queryStr = JSON.stringify(queryStringObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    this.mongooseQuery = this.mongooseQuery.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      console.log("sort", queryString.sort);
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.mongooseQuery = this.mongooseQuery.sort(sortBy);
+    } else {
+      this.mongooseQuery = this.mongooseQuery.sort("createdAt");
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.mongooseQuery.select(fields);
+    } else {
+      this.mongooseQuery = this.mongooseQuery.select("-__v");
+    }
+    return this;
+  }
+
+  search(modelName) {
+    let qeury = {};
+    if (this.queryString.keyword) {
+      if (modelName === "User") {
+        qeury = {
+          $or: [{ name: { $regex: this.queryString.keyword, $options: "i" } }],
+        };
+      } else {
+        query = {
+          $or: [
+            { name: { $regex: this.queryString.keyword, $options: "i" } },
+            {
+              description: { $regex: this.queryString.keyword, $options: "i" },
+            },
+          ],
+        };
+      }
+      this.mongooseQuery = this.mongooseQuery.and([query]);
+    }
+    return this;
+  }
+
+  paginate(countDocuments) {
+    const page = +this.queryString.page || 1;
+    const limit = +this.queryString.limit || 20;
+    const skip = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const pagination = {};
+    pagination.currentPage = page;
+    pagination.limit = limit;
+    pagination.NoOfPages = Math.ceil(countDocuments / limit);
+
+    // next pate
+    if (endIndex < countDocuments) {
+      pagination.next = page + 1;
+    }
+
+    //prev
+    if (skip > 0) {
+      pagination.prev = page - 1;
+    }
+
+    this.mongooseQuery = this.mongooseQuery.skip(skip).limit(limit);
+    this.paginationResult = pagination;
+
+    return this;
+  }
+}
+
+module.exports = ApiFeatures;

b2e8bf94a551a74150a54b17ce91597cdbde6be8
Amira Alzaian <amiraelzaian204@gmail.com>
2026-09-17 04:23:30 +0300

    Replace nodemailer with Resend for email sending
    Refactor sendEmail function to use Resend service instead of nodemailer.

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,36 +1,24 @@
-const nodemailer = require("nodemailer");
-const dns = require("dns").promises;
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (options) => {
   console.log("sendEmail called with:", options.email);
 
-  const addresses = await dns.resolve4(process.env.EMAIL_HOST);
-  const ipv4Address = addresses[0];
-
-  console.log("Resolved", process.env.EMAIL_HOST, "to IPv4:", ipv4Address);
-
-  const transporter = nodemailer.createTransport({
-    host: ipv4Address,
-    port: Number(process.env.EMAIL_PORT), 
-    secure: false, 
-    requireTLS: true, 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      servername: process.env.EMAIL_HOST,
-    },
-    connectionTimeout: 15000, 
-  });
-
   try {
-    await transporter.sendMail({
-      from: `"Bakery App" <${process.env.EMAIL_USER}>`,
+    const { data, error } = await resend.emails.send({
+      from: "Bakery App <process.env.EMAIL_USER>", 
       to: options.email,
       subject: options.subject,
       html: options.message,
     });
+
+    if (error) {
+      console.error("SEND EMAIL ERROR:", error);
+      throw new Error(error.message);
+    }
+
+    console.log("Email sent successfully:", data);
   } catch (err) {
     console.error("SEND EMAIL ERROR:", err);
     throw err;

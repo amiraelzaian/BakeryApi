0396a05f15151a595466ec2cf3baee2d2ec1f152
Amira Alzaian <amiraelzaian204@gmail.com>
2026-09-17 04:03:03 +0300

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,36 +1,19 @@
-// const nodemailer = require("nodemailer");
-
-// const sendEmail = async (options) => {
-//   const transporter = nodemailer.createTransport({
-//     host: process.env.EMAIL_HOST,
-//     port: process.env.EMAIL_PORT,
-//     secure: true,
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
-
-//   await transporter.sendMail({
-//     from: `"Bakery App" <${process.env.EMAIL_USER}>`,
-//     to: options.email,
-//     subject: options.subject,
-//     html: options.message,
-//   });
-// };
-
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
 const sendEmail = async (options) => {
   console.log("sendEmail called with:", options.email);
+
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
     secure: Number(process.env.EMAIL_PORT) === 465,
-     family: 4,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
+    },
+    lookup: (hostname, opts, callback) => {
+      dns.lookup(hostname, { family: 4 }, callback);
     },
   });
 
@@ -42,11 +25,9 @@ const sendEmail = async (options) => {
       html: options.message,
     });
   } catch (err) {
-    console.error("SEND EMAIL ERROR:", err); // ← add this
-    throw err; // re-throw so the controller's catch still handles it
+    console.error("SEND EMAIL ERROR:", err);
+    throw err;
   }
 };
 
 module.exports = sendEmail;
-
-
